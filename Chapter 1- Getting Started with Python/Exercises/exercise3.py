635471b061cfcd94516611a0fc109d19256f722f
zainabfatima04 <144207551+zainabfatima04@users.noreply.github.com>
2023-10-10 18:57:51 +0400

    exercise3.py

--- Chapter 1- Getting Started with Python/Exercises/exercise3.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise3.py
@@ -1,3 +1,16 @@
+#Write a Python program to display the current date and time.
+
+#Importing datetime module for now()
 import datetime
-current_time=datetime.datetime.now()
-print("time now at ras al khaimah is:",current_time)
+
+#Using now() to get current time
+current_time = datetime.datetime.now()
+
+#Printing attributes of now()
+print("The attribute of now() are")
+print("Year :", current_time.year)
+print("Month :", current_time.month)
+print("Day :", current_time.day)
+print("Hour :", current_time.hour)
+print("Minute :", current_time.minute)
+print("Second :", current_time.second)
