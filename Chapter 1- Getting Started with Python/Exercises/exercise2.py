40096cf1f16435a4919f2092781f8299b1968367
zainabfatima04 <144207551+zainabfatima04@users.noreply.github.com>
2023-10-10 18:57:10 +0400

    exercise2

--- Chapter 1- Getting Started with Python/Exercises/exercise2.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise2.py
@@ -1,1 +1,7 @@
-platform.python_version()
+#Write a Python program to get the Python version you are using.
+import sys
+
+print("Python version")
+print(sys.version)
+print("Version.info")
+print(sys.version_info)
