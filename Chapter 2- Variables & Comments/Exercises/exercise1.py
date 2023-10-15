4bf2867a38429122bc491cd1f05e22079553d951
zainabfatima04 <144207551+zainabfatima04@users.noreply.github.com>
2023-10-15 10:42:01 +0400

    exercise1

---
+++ Chapter 2- Variables & Comments/Exercises/exercise1.py
@@ -0,0 +1,13 @@
+#Assign a message to a variable, and print that message. Then change the value of the variable to a new message, and print the new message.
+
+#Assign a message to a variable
+var_1 = "This is the original message"
+
+#Print the original message
+print(var_1)
+
+#Change the value of the variable to a new message
+var_1 = "This is the updated message"
+
+#Print the new message
+print(var_1)
