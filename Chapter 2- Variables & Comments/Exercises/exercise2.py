e4fd014762cf2303b806a5d1cdc0f64cdc0ea060
zainabfatima04 <144207551+zainabfatima04@users.noreply.github.com>
2023-10-16 17:32:59 +0400

    exercise 2

---
+++ Chapter 2- Variables & Comments/Exercises/exercise2.py
@@ -0,0 +1,8 @@
+#Find a quote from a famous person you admire. Print the quote and the name of its author.
+
+#Quote from Eleanor Roosevelt
+quote = "The future belongs to those who believe in the beauty of their dreams."
+author = "Eleanor Roosevelt"
+
+#Print the quote and its author
+print(f"{author} once said, \"{quote}\"")
