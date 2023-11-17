256dbe60a460c9ce0d77fc0d36f19127c1153abe
zainabfatima04 <144207551+zainabfatima04@users.noreply.github.com>
2023-11-18 02:44:17 +0400

    Chapter 3

--- Chapter 3- Data Structures/Exercises/Exercise 1 - Names.py
+++ Chapter 3- Data Structures/Exercises/Exercise 1 - Names.py
@@ -1,4 +1,5 @@
-#Store the names of a few of your friends in a list called names. Print each person’s name by accessing each element in the list, one at a time.
+#Store the names of a few of your friends in a list called names. 
+#Print each person’s name by accessing each element in the list, one at a time.
 
 #Define a list of friends' names
 names = ["Aisha", "Sahiba", "Suriya", "Urooj"]
