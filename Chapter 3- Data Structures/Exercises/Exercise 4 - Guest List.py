102f7d7e4c9dc240041f78c7fa8a43c8dc938897
zainabfatima04 <144207551+zainabfatima04@users.noreply.github.com>
2023-11-17 17:14:44 +0400

    Chapter 3

---
+++ Chapter 3- Data Structures/Exercises/Exercise 4 - Guest List.py
@@ -0,0 +1,10 @@
+#If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+#Define a list of people to invite to dinner
+guests = ["Audrey Hepburn", "Coco Chanel", "Marie Curie"]
+
+#Print invitation messages to each person
+for person in guests:
+    invitation = f"Dear {person},\n\tYou are cordially invited to join me for dinner. It would be an honor to have your company.\nSincerely, Zainab Fatima"
+    print(invitation)
+    print("\n")
