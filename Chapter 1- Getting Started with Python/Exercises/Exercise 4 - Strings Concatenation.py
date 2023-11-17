3be9f25bd85ccc8e6622e43ca9d43672f7781fa5
zainabfatima04 <144207551+zainabfatima04@users.noreply.github.com>
2023-11-18 02:06:46 +0400

    Chapter 1

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 4 - Strings Concatenation.py
@@ -0,0 +1,12 @@
+#Write three strings in different variables and print the output as one string.
+
+# Define three strings in different variables
+string1 = "Hello"
+string2 = " from"
+string3 = " Zainab!"
+
+# Combine the strings into one string
+combined_string = string1 + string2 + string3
+
+# Print the result
+print(combined_string)
