d565857f460a3ae15373ba34c340ddad4d69a6dc
zainabfatima04 <144207551+zainabfatima04@users.noreply.github.com>
2023-11-18 02:46:41 +0400

    Chapter 3

--- Chapter 3- Data Structures/Exercises/Exercise 3 - Your Own List.py
+++ Chapter 3- Data Structures/Exercises/Exercise 3 - Your Own List.py
@@ -1,4 +1,5 @@
-#Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+#Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. 
+#Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
 #Define a list of favorite motorcycles
 motorcycles = ["Yamaha", "Ducati", "Kawasaki", "Harley-Davidson", "Honda"]
 
