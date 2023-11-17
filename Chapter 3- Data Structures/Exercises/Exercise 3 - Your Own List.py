144acc4a2879c03cb767fe530f1198482794c228
zainabfatima04 <144207551+zainabfatima04@users.noreply.github.com>
2023-11-17 17:04:59 +0400

    Chapter 3

---
+++ Chapter 3- Data Structures/Exercises/Exercise 3 - Your Own List.py
@@ -0,0 +1,8 @@
+#Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+#Define a list of favorite motorcycles
+motorcycles = ["Yamaha", "Ducati", "Kawasaki", "Harley-Davidson", "Honda"]
+
+#Print statements about each motorcycle
+for brand in motorcycles:
+    statement = f"I would like to own a {brand} motorcycle."
+    print(statement)
