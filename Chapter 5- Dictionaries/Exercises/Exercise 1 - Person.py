ec4adc8a2044f49b2c1de460cbe9659f81ffcd33
zainabfatima04 <144207551+zainabfatima04@users.noreply.github.com>
2023-11-18 17:53:47 +0400

    Chapter 5

--- Chapter 5- Dictionaries/Exercises/Exercise 1 - Person.py
+++ Chapter 5- Dictionaries/Exercises/Exercise 1 - Person.py
@@ -1,4 +1,7 @@
-#Use a dictionary to store information about a person you know. Store their first name, last name, age, and the city in which they live. You should have keys such as first_name, last_name, age, and city. Print each piece of information stored in your dictionary.
+#Use a dictionary to store information about a person you know. 
+#Store their first name, last name, age, and the city in which they live. 
+#You should have keys such as first_name, last_name, age, and city. 
+#Print each piece of information stored in your dictionary.
 
 #Create a dictionary to store information about a person
 person_info = {
