6189b8abd316e1b22edf843b52912011fadf3d34
zainabfatima04 <144207551+zainabfatima04@users.noreply.github.com>
2023-11-18 18:19:15 +0400

    Chapter 2

--- Chapter 2- Variables & Comments/Exercises/Exercise 5 - USB Shopper.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise 5 - USB Shopper.py
@@ -1,4 +1,6 @@
-#A girl heads to a computer shop to buy some USB sticks. She loves USB sticks and wants as many as she can get for £50. They are £6 each.
+#A girl heads to a computer shop to buy some USB sticks. 
+#She loves USB sticks and wants as many as she can get for £50. 
+#They are £6 each.
 #Write a program that calculates how many USB sticks she can buy and how many pounds she will have left.
 
 #Cost of each USB stick
