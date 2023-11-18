70c725dbebd637aea35791f6cdf41a4f6696f43a
zainabfatima04 <144207551+zainabfatima04@users.noreply.github.com>
2023-11-18 19:07:55 +0400

    Chapter 4

--- Chapter 4- Control Flow/Exercises/Exercise 3 - Alien Colors#3.py
+++ Chapter 4- Control Flow/Exercises/Exercise 3 - Alien Colors#3.py
@@ -1,4 +1,4 @@
-#Turn your if-else chain from Exercise 5-4 into an if-elif else chain.
+#Turn your if-else chain from Exercise 5-4 into an if-elif-else chain.
 #•If the alien is green, print a message that the player earned 5 points.
 #•If the alien is yellow, print a message that the player earned 10 points.
 #•If the alien is red, print a message that the player earned 15 points.
